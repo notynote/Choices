dfaa9827db04dcd7f4d56197c06adb5c614c3ccc
notynote <32334875+notynote@users.noreply.github.com>
2021-03-11 21:25:15 +0700

    Finish

--- src/choices/Main.java
+++ src/choices/Main.java
@@ -45,4 +45,6 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    //--module-path="C:\Program Files\Java\javafx-sdk-11.0.2\lib" --add-modules=javafx.controls,javafx.fxml --add-modules javafx.controls,javafx.media
 }
